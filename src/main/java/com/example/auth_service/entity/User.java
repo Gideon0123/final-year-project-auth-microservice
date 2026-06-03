fde2a044db38c38374383282a57ec4579f0d175f
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-06-03 04:53:37 -0700

    first commit

---
+++ src/main/java/com/example/auth_service/entity/User.java
@@ -0,0 +1,73 @@
+package com.example.auth_service.entity;
+
+import com.example.auth_service.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email"),
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "phoneNo")
+        }
+)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(unique = true, nullable = false)
+    private String phoneNo;
+
+    private String department;
+
+    private String faculty;
+
+    private String institution;
+
+    @Builder.Default
+    private boolean enabled = true;
+
+    @Builder.Default
+    private boolean emailVerified = false;
+
+    @Builder.Default
+    private boolean accountNonLocked = true;
+
+    @Builder.Default
+    private int failedLoginAttempts = 0;
+
+    private LocalDateTime lockTime;
+
+    private LocalDateTime lastLoginAt;
+
+    private String profileImageUrl;
+}
---
+++ src/main/java/com/example/auth_service/enums/Role.java
@@ -0,0 +1,10 @@
+package com.example.auth_service.enums;
+
+public enum Role {
+
+    ADMIN,
+    LECTURER,
+    REVIEWER,
+    RESEARCHER,
+    STUDENT
+}
