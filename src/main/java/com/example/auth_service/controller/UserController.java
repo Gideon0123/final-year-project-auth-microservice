fb9e40ad70051c79b5037ead22d3d7805edb9301
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-08-19 03:20:44 +0100

    Feign changes 3

--- src/main/java/com/example/auth_service/controller/UserController.java
+++ src/main/java/com/example/auth_service/controller/UserController.java
@@ -10,9 +10,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/users")
